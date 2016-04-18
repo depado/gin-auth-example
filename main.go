979eafde64da4773152cd13f25a9bdcc5238e7df
Depado <paul.lhussiez@gmail.com>
2016-04-18 20:26:00 +0200

    Base code

---
+++ main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/contrib/sessions"
+	"github.com/gin-gonic/gin"
+)
+
+// Thanks to otraore for the code example
+// https://gist.github.com/otraore/4b3120aa70e1c1aa33ba78e886bb54f3
+
+func main() {
+	r := gin.Default()
+	store := sessions.NewCookieStore([]byte("secret"))
+	r.Use(sessions.Sessions("mysession", store))
+
+	r.POST("/login", login)
+	r.GET("/logout", logout)
+	private := r.Group("/private")
+	{
+		private.GET("/", private)
+		private.GET("/two", private2)
+	}
+	private.Use(AuthRequired())
+	r.Run(":8080")
+}
+
+func AuthRequired() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		session := sessions.Default(c)
+		user := session.Get("user")
+		if user == nil {
+			// You'd normally redirect to login page
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid session token"})
+		} else {
+			// Continue down the chain to handler etc
+			c.Next()
+		}
+	}
+}
+
+func login(c *gin.Context) {
+	session := sessions.Default(c)
+	username := c.PostForm("username")
+	password := c.PostForm("password")
+
+	if strings.Trim(username, " ") == "" || strings.Trim(password, " ") == "" {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Parameters can't be empty"})
+		return
+	}
+	if username == "hello" && password == "itsme" {
+		session.Set("user", username) //In real world usage you'd set this to the users ID
+		err := session.Save()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to generate session token"})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"message": "Successfully authenticated user"})
+		}
+	} else {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Authentication failed"})
+	}
+}
+
+func logout(c *gin.Context) {
+	session := sessions.Default(c)
+	user := session.Get("user")
+	if user == nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid session token"})
+	} else {
+		log.Println(user)
+		session.Delete("user")
+		session.Save()
+		c.JSON(http.StatusOK, gin.H{"message": "Successfully logged out"})
+	}
+}
+
+func private(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"hello": user})
+}
+
+func private2(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"hello": "Logged in user"})
+}
